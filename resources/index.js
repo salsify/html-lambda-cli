f80ad630a07b85ed6822faa77e7822154238b8a5
Cameron Wheeler <cwheeler@salsify.com>
2016-06-08 10:21:30 -0400

    Fixed resources indentation

--- resources/index.js
+++ resources/index.js
@@ -1,5 +1,5 @@
 const runner = require('html-lambda-runner');
 
 exports.handler = (event, context, callback) => {
-	runner.run(event, context, callback);
+  runner.run(event, context, callback);
 };
--- resources/templateData.js
+++ resources/templateData.js
@@ -1,5 +1,5 @@
 //Returns an object whose properties will be injected into the template
 
 module.exports = (context, event) => {
-	return {heading: 'HTML Lambda'};
+  return {heading: 'HTML Lambda'};
 }
